9000b1e0471da6c49926c7f6214d4743908f00f3
Jibin George <george6@avaya.com>
2019-07-01 12:28:33 +0530

    Taking input from console arguments

--- C#/avayacloud-client/AvayacloudSampleClient.cs
+++ C#/avayacloud-client/AvayacloudSampleClient.cs
@@ -6,15 +6,35 @@ namespace AvayaCloudClient
 {
     public  class AvayacloudSampleClient
     {
-        private static readonly string agent_username = "TestAgent2";
+        private static readonly string agent_username = "TestAgent3";
         private static readonly string agent_password = "Spoken@1";
-        private static readonly string endpoint = "http://192.168.0.101:8086";
-        private static readonly string abcusername = "prov1";
-        private static readonly string abcpassword = "Avaya123$";
+        private static string endpoint = "http://148.147.152.171:8086";
+        private static string abcusername = "prov1";
+        private static string abcpassword = "Avaya123$";
         static async Task Main(string[] args)
         {
             //avayacloud_client.Session session = new avayacloud_client.Session("https://login.bpo.avaya.com", "StoreAPIProvisioner", "WhHh3xG*us");
             //avayacloud_client.Session session = new avayacloud_client.Session("https://integration.bpo.avaya.com", "PuneDevSystemUser", "Avaya123$");
+            if(args.Length == 0)
+            {
+                Console.WriteLine("No command line arguments. So contiuning with defined values");
+            }
+            else
+            {
+                if(args[0] != null)
+                {
+                    endpoint = args[0];
+                }
+                if (args[1] != null)
+                {
+                    abcusername = args[1];
+                }
+                if (args[2] != null)
+                {
+                    abcpassword = args[2];
+                }
+            }
+            Console.WriteLine("Endpoint used:- " + endpoint);
             AvayaCloudClient.Session session = new AvayaCloudClient.Session(endpoint, abcusername, abcpassword);
             session.creatSessionParameters();
             Console.WriteLine("************Creation of Agent*****************");
