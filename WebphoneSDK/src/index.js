b1a6991fc591ecf6d59f560d7d9d9d2989a66b92
Pankaj Gupta <pankaj.gupta@spoken.com>
2020-03-10 13:12:30 -0700

    Generate source map

--- WebphoneSDK/src/index.js
+++ WebphoneSDK/src/index.js
@@ -1,7 +1,3 @@
-import sample from "./sample.js";
 import { state, commandToWebPhone } from "./test.js";
 
-
- console.log("abc :", sample.abc());
- console.log("xyz :", sample.xyz());
-export { sample, state, commandToWebPhone };
+export { state, commandToWebPhone };
--- WebphoneSDK/webpack.config.js
+++ WebphoneSDK/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 
 module.exports = {
   entry: './src/index.js',
+  devtool: 'source-map',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
